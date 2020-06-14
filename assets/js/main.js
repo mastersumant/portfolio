5e83079fb724e99d53e5062868d4219cbe382df1
Sumant Kr <mastersumantkvs@gmail.com>
2020-06-14 08:28:23 +0530

    added navvar and hamburger

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,3 +65,14 @@ $('a[href*="#"]')
 
 
 
+  $(document).ready(function(){
+    $(".button a").click(function(){
+        $(".overlay").fadeToggle(200);
+       $(this).toggleClass('btn-open').toggleClass('btn-close');
+    });
+});
+$('.overlay').on('click', function(){
+    $(".overlay").fadeToggle(200);   
+    $(".button a").toggleClass('btn-open').toggleClass('btn-close');
+    open = false;
+});
