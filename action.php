7a466cda3128ebae3243441a598e48ed23dcc45d
Sumant Kr <mastersumantkvs@gmail.com>
2020-06-25 22:09:27 +0530

    auto confirmation mail in action.php added and form validation in Js

--- action.php
+++ action.php
@@ -1,25 +1,39 @@
 <?php
 $name = filter_input(INPUT_POST, 'name');
 $mail = filter_input(INPUT_POST, 'email');
+$phone = filter_input(INPUT_POST, 'phone');
 $message = filter_input(INPUT_POST, 'message');
-if (!empty($name)){
-if (!empty($mail)){
-if (!empty($message)){
+
+if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+    echo "Invalid email format";
+  }
 $host = "localhost";
-$dbusername = "root";
-$dbpassword = "%m_M4i6KEO[h([BZ";
-$dbname = "portfolio";
-// Create connection
+$dbusername = "";
+$dbpassword = "";
+$dbname = "";
+    // Create connection
 $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
 if (mysqli_connect_error()){
 die('Connect Error ('. mysqli_connect_errno() .') '
 . mysqli_connect_error());
 }
 else{
-$sql = "INSERT INTO personal (name, email, message)
-values ('$name','$mail','$message')";
+$sql = "INSERT INTO portfolio (name, email, phone, message)
+values ('$name','$mail','$phone','$message')";
 if ($conn->query($sql)){
-echo "New record is inserted sucessfully";
+ $body="Hii {$name},\n\n";
+ $body.="Thank you for getting in touch! \n\nI have received your message and would like to thank you for writing to me. \n\nIf your inquiry is urgent, please use the contact number below to talk to me. \n\nOtherwise, I will reply by email as soon as possible.";
+ $body.="\n\nThe contact information shared by you :\n\n";
+ $body.="Name : {$name}\n";
+ $body.="Email : {$mail}\n";
+ $body.="Contact : {$phone}\n";
+ $body.="\nTalk to you soon,\n      ~Sumant Kr.";
+ $body.="\n\nContact : +917619481923";
+ $subject="Thanks for your contact";	        	        
+ $from = "sumant@sitsark.in";
+ $headers = "From:" . $from;
+ mail($mail,$subject,$body,$headers);
+header("Location: http://portfolio.sitsark.in/");
 }
 else{
 echo "Error: ". $sql ."
@@ -27,19 +41,6 @@ echo "Error: ". $sql ."
 }
 $conn->close();
 }
-}
-else{
-    echo "Message should not be empty";
-    die();
-    }
-}
-else{
-echo "E-mail should not be empty";
-die();
-}
-}
-else{
-echo "Name should not be empty";
-die();
-}
+
+
 ?>
--- assets/js/main.js
+++ assets/js/main.js
@@ -100,3 +100,44 @@ function toggle() {
   }
 }
 
+//valid form 
+
+function valid()                                    
+{ 
+    var name = document.forms["RegForm"]["name"];               
+    var email = document.forms["RegForm"]["email"];    
+    var phone = document.forms["RegForm"]["phone"];  
+    var message =  document.forms["RegForm"]["message"];  
+      
+   
+    if (name.value == "")                                  
+    { 
+        window.alert("Please enter your name."); 
+        name.focus(); 
+        return false; 
+    } 
+       
+    if (email.value == "")                                   
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (phone.value == "")                           
+    { 
+        window.alert("Please enter your telephone number."); 
+        phone.focus(); 
+        return false; 
+    } 
+   
+    if (message.value == "")                        
+    { 
+        window.alert("Please enter your message"); 
+        message.focus(); 
+        return false; 
+    } 
+   
+   
+    return true; 
+}
